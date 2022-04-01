e5e42ec9204d4961765426594bd48970c8ccec48
kresimirspehar <kresimirspehar5@gmail.com>
2022-04-01 10:07:05 +0200

    commit

---
+++ src/components/Kartica.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout row wrap style>
+    <v-card
+      class="text-xs-center ma-3"
+      max-width="500"
+      style="margin-top: 100px margin-left: 20px"
+    >
+      <router-link
+        style="text decoration:none; color:black;"
+        :to="{ name: 'About', params: { isbn: id } }"
+      >
+        <v-card-title> Naziv: {{ naziv }} </v-card-title>
+      </router-link>
+      <v-card-text> Autor: {{ aut[0] }} </v-card-text>
+      <v-card-text> Datum: {{ abt }} </v-card-text>
+      <v-card-actions>
+        <router-link :to="{ name: 'About', params: { isbn: id } }">
+          <v-btn rounded color="primary" dark>DETALJI</v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
+</template>
+<script>
+export default {
+  name: "Kartica",
+  props: {
+    id: String,
+    abt: String,
+    naziv: String,
+    aut: [],
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container style="margin-top: 10px; margin-bottom: 10px">
+    <h1
+      class="white--text mb-10 display-1 text-xs-center"
+      style=" margin-bottom: 20px font-weight: 500; text-shadow: 3px 2px #000000"
+    >
+      WA - Blic
+    </h1>
+    <v-layout row wrap style>
+      <kartica
+        v-for="card in card"
+        :id="card.isbn"
+        :abt="card.name"
+        :naziv="card.name"
+        :aut="card.authors"
+        :key="card.isbn"
+      />
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Kartica from "@/components/Kartica.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    Kartica,
+  },
+  data() {
+    return {
+      card: [],
+    };
+  },
+  async mounted() {
+    const temp = await axios.get("https://www.anapioficeandfire.com/api/books");
+    this.card = temp.data;
+    console.log(this.card);
+  },
+};
+</script>
